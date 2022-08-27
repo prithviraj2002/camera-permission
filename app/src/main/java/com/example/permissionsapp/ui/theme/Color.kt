45b1e343d07e3c30bec93cdf44e8fb543ee8182a
Raj <rajashekarraju123@gmail.com>
2022-08-27 21:14:17 +0530

    Added reusable theme design and colors from compose actors app

--- app/src/main/java/com/example/permissionsapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/permissionsapp/ui/theme/Color.kt
@@ -2,7 +2,16 @@ package com.example.permissionsapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val light_primary = Color(0xFFaa370c)
+val light_onPrimary = Color(0xFFffffff)
+val light_background = Color(0xFFFFFAF9)
+val light_onBackground = Color(0xFF211a18)
+val light_surface = Color(0xFFFFE6DB)
+val light_onSurface = Color(0xFF211a18)
+
+val dark_primary = Color(0xFFffb59c)
+val dark_onPrimary = Color(0xFF5f1600)
+val dark_background = Color(0xFF211a18)
+val dark_onBackground = Color(0xFFede0dc)
+val dark_surface = Color(0xFF302522)
+val dark_onSurface = Color(0xFFede0dc)
